eb880fecded3a590c896f7b6d21be6a3b60143fc
aliasqardehqan <aliasqar.dehqanii@gmail.com>
2024-01-01 20:33:06 +0330

    feat(callapi): The API calling code is complete

--- callapi.py
+++ callapi.py
@@ -1,31 +1,32 @@
-"""
-    This code for call api from master project.
-"""
-
 import requests
 from exceptions import APIException
 
 
-def call_api(url):
+
+def call_api_with_body(url, api_token, channel_id):
     try:
-        response = requests.get(url)
-        
+        headers = {'Authorization': f'Bearer {api_token}'}
+        payload = {'channel_id': channel_id}
+
+        response = requests.post(url, headers=headers, data=payload)
+
         if response.status_code == 200:
-            print("API Response:")
-            print(response.text)
+            return response.text
         else:
             raise APIException(f"Error: Unable to call API. Status Code: {response.status_code}")
-        
+
     except requests.exceptions.RequestException as req_exception:
         print(f"Request Exception: {req_exception}")
-        
+
     except APIException as custom_exception:
         print(custom_exception)
-    
+
     except Exception as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
+    url = 'api_url'
+    api_token = 'token'  
+    channel_id = ''
     
-    api_url = 'http://178.131.94.27:13123/api/device/check-value-sensor/'
-    call_api(api_url)
+    call_api_with_body(url, api_token, channel_id)
