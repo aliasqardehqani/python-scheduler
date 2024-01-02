c0f829a6cd29e82bafd24c85f8fa95a604dede31
aliasqardehqan <aliasqar.dehqanii@gmail.com>
2024-01-02 13:27:07 +0330

    feat(master): Somthing changed.

--- api/callapi.py
+++ api/callapi.py
@@ -1,6 +1,10 @@
 import requests
-from exception.exceptions import APIException
+import json
+# import sys
+# print(sys.path)
 
+from exceptions import APIException
+from converter import convert_json_to_data
 
 
 def call_api_with_body(url, api_token, channel_id):
@@ -11,7 +15,8 @@ def call_api_with_body(url, api_token, channel_id):
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            return response.text
+            print(response.text)
+            return convert_json_to_data(response.text)
         else:
             raise APIException(f"Error: Unable to call API. Status Code: {response.status_code}")
 
@@ -25,8 +30,13 @@ def call_api_with_body(url, api_token, channel_id):
         print(f"Error: {e}")
 
 if __name__ == "__main__":
-    url = 'api_url'
+    
+    url = 'api_urls'
     api_token = 'token'  
-    channel_id = ''
+    channel_id = 'channelID'
     
     call_api_with_body(url, api_token, channel_id)
+
+
+
+# {"Data":[{"id":3,"date":"1402-09-16","start_time":"10:30:00","end_time":"17:30:00"}],"Message":null}
